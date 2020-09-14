9206f1600cc3985d1898a6e50dabd5ad6beb14d6
Julio Nico <julionico@pop-os.localdomain>
2020-09-14 15:51:26 -0300

    Primeiro arquivo

---
+++ examples/and_gate.vhd
@@ -0,0 +1,23 @@
+-- Disciplina: Sistemas Digitais
+-- Exemplo de uma porta and
+-- Autor: Julio Nico Dantas dos Santos
+
+-- Bibliotecas e pacotes
+
+-- Entidade (Entidade basicamente são as descrições de interface com o mundo externo. conectores de entrada e saída das portas lógicas.)
+
+entity and_gate is
+    port (
+        A, B:   in  bit; --Entradas 
+        X:      out bit  --Saídas   
+    );
+end entity and_gate;
+
+-- Arquitetura
+
+architecture main of and_gade is
+begin
+    
+    Z <= a and b;
+
+end architecture main;
