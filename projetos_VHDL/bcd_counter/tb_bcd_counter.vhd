65ca524f9e57bcfc0beee94d46ec2e6e7c81186e
Julio Nico <julionicodantas@gmail.com>
2020-10-24 11:30:22 -0300

    contador BCD inc/dec

---
+++ projetos_VHDL/bcd_counter/tb_bcd_counter.vhd
@@ -0,0 +1,59 @@
+--------------------------------------------------------------------------------
+-- Disciplina: Sistemas Digitais
+-- Circuito Contador Binário.
+-- Autor: Julio Nico Dantas dos Santos
+-------------------------------------------
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+ENTITY tb_counter IS
+END tb_counter;
+ 
+ARCHITECTURE behavior OF tb_counter IS 
+	constant n  :   integer := 4; -- quantidade de bits do contador (DUT)
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal rst : std_logic := '0';
+
+ 	--Outputs
+   signal q : std_logic_vector(3 downto 0) := (others => '0');
+
+   -- Clock period definitions
+   constant clk_period : time := 20 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+    counter_unit : entity work.counter(arch)
+	 generic map(n)
+	 port map(clk, rst, q);
+
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+
+   --**************************
+   -- reset
+   --**************************
+   -- reset asserted for clk_period/2
+   rst <= '1', '0' after clk_period/2;
+
+	-- Bateria de testes
+   Battery_tests :process
+   begin
+		wait for 210ns;
+		-- terminate simulation
+      assert false
+         report "Simulation Completed"
+         severity failure;
+   end process Battery_tests;
+
+END;
---
+++ projetos_VHDL/bcd_inc_dec/bcd_inc_dec.vhd
@@ -0,0 +1,52 @@
+-- Disciplina: Sistemas Digitais
+-- Circuito Contador Binário.
+-- Autor: Julio Nico Dantas dos Santos
+-------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity counter is
+    generic(n   :   integer := 4);  -- Indica a quantidade de bits do contador
+    port (
+        clk, rst, inc, dec    : in std_logic;
+        q           : out std_logic_vector(n-1 downto 0)
+        );
+end counter;
+
+architecture arch of counter is
+
+    signal q_temp   :   unsigned(n-1 downto 0);
+
+begin
+
+    contagem : process(clk, rst, inc, dec)
+    begin
+        -- reset assincrono
+        if rst = '1' then
+            q_temp <= (others => '0');
+        elsif RISING_EDGE(clk) then
+				--incrementa
+				if inc = '1' then
+					if q_temp /= 9 then
+						q_temp <= q_temp +1;
+					else       -- unidade XX9
+						q_temp <= (others => '0'); 
+					end if;
+					
+				--decrementa
+				elsif dec = '1' then
+					if q_temp /= 0 then
+						q_temp <= q_temp -1;
+					else       -- unidade XX9
+						q_temp <= "1001";
+					end if;
+				end if;
+				
+        end if;        
+    end process contagem;
+
+    q <= std_logic_vector(q_temp);
+
+end arch; 
