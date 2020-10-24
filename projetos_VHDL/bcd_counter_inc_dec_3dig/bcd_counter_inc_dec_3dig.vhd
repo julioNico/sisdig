93de6da2c0ed7f13d54f1530b26bed957827488a
Julio Nico <julionicodantas@gmail.com>
2020-10-24 13:58:26 -0300

    Contador BCD de 3 bits Completo

---
+++ projetos_VHDL/bcd_counter_inc_dec_3dig/bcd_counter_inc_dec_3dig.vhd
@@ -0,0 +1,98 @@
+-- Disciplina: Sistemas Digitais
+-- Circuito Contador Binário.
+-- Autor: Julio Nico Dantas dos Santos
+-------------------------------------------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+ENTITY counter IS
+    GENERIC (n : INTEGER := 4); -- Indica a quantidade de bits do contador
+    PORT (
+        clk, reset, inc, dec : IN STD_LOGIC;
+        bcd2, bcd1, bcd0 : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0)
+    );
+END counter;
+
+ARCHITECTURE arch OF counter IS
+
+    SIGNAL q0_reg : unsigned(n - 1 DOWNTO 0);
+    SIGNAL q1_reg : unsigned(n - 1 DOWNTO 0);
+    SIGNAL q2_reg : unsigned(n - 1 DOWNTO 0);
+	 SIGNAL inc_ant: BIT;
+	 SIGNAL dec_ant: BIT;	 
+BEGIN
+
+    contagem : PROCESS (clk, reset, inc, dec)
+    BEGIN
+        -- reset assincrono
+        IF inc = '1' and dec = '0' THEN
+        inc_ant <= '1';
+        dec_ant <= '0';
+        ELSIF inc = '0' and dec = '1' THEN
+        inc_ant <= '0';
+        dec_ant <= '1';
+        END IF;        
+        -- reset assincrono
+        IF reset = '1' THEN
+            q0_reg <= (OTHERS => '0');
+            q1_reg <= (OTHERS => '0');
+            q2_reg <= (OTHERS => '0');
+        ELSIF RISING_EDGE(clk) THEN
+            --incrementa
+            IF inc_ant = '1' THEN
+
+                -- unidade XX9
+                IF q0_reg /= 9 THEN
+                    q0_reg <= q0_reg + 1;
+                ELSE
+                    q0_reg <= (OTHERS => '0');
+
+                    -- unidade X99
+                    IF q1_reg /= 9 THEN
+                        q1_reg <= q1_reg + 1;
+                    ELSE
+                        q1_reg <= (OTHERS => '0');
+
+                        -- unidade 999
+                        IF q2_reg /= 9 THEN
+                            q2_reg <= q2_reg + 1;
+                        ELSE
+                            q2_reg <= (OTHERS => '0');
+                        END IF;
+                    END IF;
+                END IF;
+            END IF;
+                --decrementa
+                -------------------
+                IF dec_ant = '1' THEN
+
+                    -- unidade XX9
+                    IF q0_reg /= 0 THEN
+                        q0_reg <= q0_reg - 1;
+                    ELSE
+                        q0_reg <= "1001";
+
+                        -- unidade X99
+                        IF q1_reg /= 0 THEN
+                            q1_reg <= q1_reg - 1;
+                        ELSE
+                            q1_reg <= "1001";
+
+                            -- unidade 999
+                            IF q2_reg /= 0 THEN
+                                q2_reg <= q2_reg - 1;
+                            ELSE
+                                q2_reg <= "1001";
+                            END IF;
+                        END IF;
+                    END IF;
+                END IF;            
+        END IF;
+    END PROCESS contagem;
+
+    bcd0 <= STD_LOGIC_VECTOR(q0_reg);
+    bcd1 <= STD_LOGIC_VECTOR(q1_reg);
+    bcd2 <= STD_LOGIC_VECTOR(q2_reg);
+END arch;
---
+++ projetos_VHDL/bcd_counter_inc_dec_3dig/tb_bcd_counter_inc_dec_3dig.vhd
@@ -0,0 +1,72 @@
+--------------------------------------------------------------------------------
+-- Disciplina: Sistemas Digitais
+-- Circuito Contador Binário.
+-- Autor: Julio Nico Dantas dos Santos
+-------------------------------------------
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+ENTITY tb_counter IS
+END tb_counter;
+ 
+ARCHITECTURE behavior OF tb_counter IS 
+	constant n  :   integer := 4; -- quantidade de bits do contador (DUT)
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal reset : std_logic := '0';
+	signal inc : std_logic := '0';
+	signal dec : std_logic := '0';
+
+ 	--Outputs
+   signal bcd0 : std_logic_vector(3 downto 0) := (others => '0');
+   signal bcd1 : std_logic_vector(3 downto 0) := (others => '0');
+	signal bcd2 : std_logic_vector(3 downto 0) := (others => '0');
+	
+   -- Clock period definitions
+   constant clk_period : time := 5 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+    counter_unit : entity work.counter(arch)
+	 generic map(n)
+	 port map(clk, reset, inc, dec, bcd2, bcd1, bcd0);
+
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+
+   --**************************
+   -- reset
+   --**************************
+   -- reset asserted for clk_period/2
+   reset <= '1', '0' after clk_period/2;
+
+	-- Bateria de testes
+   Battery_tests :process
+   begin
+		inc <= '1'; 
+		dec <= '0';		
+		wait for 5us;
+		inc <= '0'; 
+		dec <= '0';		
+		wait for 5us;
+		inc <= '0'; 
+		dec <= '1';		
+		wait for 10us;
+
+		-- terminate simulation
+      assert false
+         report "Simulation Completed"
+         severity failure;
+   end process Battery_tests;
+
+END;
